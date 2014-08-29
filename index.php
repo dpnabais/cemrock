749672ec5510c78755df35d250e73090f22ed66c
dpnabais <dpnabais@gmail.com>
2014-08-29 13:47:34 -0600

    new headerbg slideshow

--- index.php
+++ index.php
@@ -41,46 +41,6 @@
 
 		<h1>Welcome to our company</h1>
 
-		<div id="slidy-container">
-			<div id="slidy">
-				<img src="img/slide-1.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-2.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-3.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-4.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-5.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-6.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-7.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-8.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-9.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-10.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-11.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-12.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-13.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-14.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-15.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-16.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-17.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-18.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-19.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-20.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-21.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-22.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-23.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-24.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-25.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-26.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-27.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-28.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-29.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-30.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-31.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-32.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-33.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-				<img src="img/slide-34.jpg" alt data-caption="&copy; Cemrock Concrete & Construction">
-			</div>
-		</div>
-
-
 
 		<p><strong>Cemrock Concrete &amp; Construction Ltd.</strong> has been involved in the cast-in-place concrete construction business since our inception in 2003.</p>
 		<p>During this time we have had the pleasure of developing successful relationships with Alberta's Construction Leaders. We conduct our business in a professional manner and have developed a reputation for providing exceptional concrete work.</p>
@@ -112,13 +72,9 @@
 
 </div>
 
-
-
 </div>
 
 
-<script src="cssslidy.js"></script>
-<script>cssSlidy();</script>
 
 </body>
 </html>
