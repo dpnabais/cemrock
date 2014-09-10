1dd57748e43c3ec48598c1ad196323a20558bb32
dpnabais <dpnabais@gmail.com>
2014-09-10 12:47:21 -0600

    minor editings

--- index.php
+++ index.php
@@ -16,15 +16,16 @@
 		<!--############################# -->
 		<!--#########  CONTENT ########## -->
 
-		<h1>Welcome to our company</h1>
-
-
-		<p><strong>Cemrock Concrete &amp; Construction Ltd.</strong> has been involved in the cast-in-place concrete construction business since our inception in 2003.</p>
-		<p>During this time we have had the pleasure of developing successful relationships with Alberta's Construction Leaders. We conduct our business in a professional manner and have developed a reputation for providing exceptional concrete work.</p>
+		<p><strong>Cemrock Concrete &amp; Construction Ltd.</strong> has been
+			involved in the cast-in-place concrete construction business since our
+			inception in 2003.</p>
+		<p>During this time we have had the pleasure of developing successful
+			relationships with Alberta's construction leaders. We conduct our
+			business in a professional manner and have developed a reputation for
+			providing exceptional concrete work.</p>
 		<p>We continue to build on our reputation through our:<br>
 		<ul>
-			<li>COR certified safety program<br>
-			(Alberta Construction Safety Association)</li>
+			<li>COR certified safety program (Alberta Construction Safety Association)</li>
 			<li>Experienced and Knowledgeable Field Personnel</li>
 			<li>Quick Response Estimating (Budgets, Bidding, Revisions)</li>
 			<li>Thorough Planning/Organization (Schedules, Submissions)</li>
