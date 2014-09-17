fcebc39cd9291bdd6df483b5000c3b36a9231e8d
dpnabais <dpnabais@gmail.com>
2014-09-17 15:35:12 -0600

    Merged Current Projects into Formwork

--- formwork.php
+++ formwork.php
@@ -3,6 +3,41 @@
 <html lang="en">
 <head>
 	<?php include 'includes/head.inc' ?>
+	<script>
+		$( document ).ready(function() {
+			$("#current-content").hide();
+				$( ".current-item" ).click(function( event ) {
+					if(this.id == "ci1"){
+						$("#current-content").empty();
+						$("#current-content").show();
+						$("#current-content").load("./current/p1/info.php");
+					}
+					if(this.id == "ci2"){
+						$("#current-content").empty();
+						$("#current-content").show();
+						$("#current-content").load("./current/p2/info.php");
+					}
+					if(this.id == "ci3"){
+						$("#current-content").empty();
+						$("#current-content").show();
+						$("#current-content").load("./current/p3/info.php");
+					}
+					if(this.id == "ci4"){
+						$("#current-content").empty();
+						$("#current-content").show();
+						$("#current-content").load("./current/p4/info.php");
+					}
+				});
+				$( ".current-item").fadeTo("slow", 0.5);
+				$( ".current-item").hover(
+					function(){
+						$(this).fadeTo( "fast", 1 );
+					}, function(){
+						$(this).fadeTo( "fast", 0.5);
+					}
+				);
+		});
+		</script>
 </head>
 
 <body class="formwork">
@@ -44,9 +79,30 @@
 		$images44 = array_values($images4);
 	?>
 
-		<h1>Formwork Projects</h1>
+		<h1>Current Formwork Projects</h1>
 
-		<a id="slink" class="icon-share" href="current.php">Current Projects</a>
+		<!--<a id="slink" class="icon-share" href="current.php">Current Projects</a>-->
+
+
+
+
+<div id="current-content">
+	<!-- leprechaun's droppings here  -->
+</div>
+<br><br>
+
+<div id="current-container">
+	<div class="current-item" id="ci1"> <img src="./current/p1/thumb.jpg"/>11 Av.<br><br></div>
+	<div class="current-item" id="ci2"> <img src="./current/p2/thumb.jpg"/>UofC/7<br><br></div>
+	<div class="current-item" id="ci3"> <img src="./current/p3/thumb.jpg"/>UofC/8<br><br></div>
+	<div class="current-item" id="ci4"> <img src="./current/p4/thumb.jpg"/>Mahogany<br><br></div>
+</div>
+
+
+
+
+
+
 
 		<p><strong>Cemrock Formwork Division</strong> has successfully completed a wide array of concrete projects including:</p>
 
