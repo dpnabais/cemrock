e53c310bc51a2b27f2af4ff300b8b2681fbd7d65
Daniel <daniel@cemrock.ca>
2023-11-08 11:02:05 -0700

    Major Update - Time Travel!

--- formwork.php
+++ formwork.php
@@ -1,108 +1,453 @@
-<!doctype html>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Cemrock Concrete &amp; Construction</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
 
-<html lang="en">
-<head>
-	<?php include 'includes/head.inc' ?>
-	<script>
-		$( document ).ready(function() {
-			$("#current-content").hide();
-				$( ".current-item" ).click(function( event ) {
-					if(this.id == "ci1"){
-						$("#current-content").empty();
-						$("#current-content").show();
-						$("#current-content").load("./current/p1/info.php");
-					}
-					if(this.id == "ci2"){
-						$("#current-content").empty();
-						$("#current-content").show();
-						$("#current-content").load("./current/p2/info.php");
-					}
-					if(this.id == "ci3"){
-						$("#current-content").empty();
-						$("#current-content").show();
-						$("#current-content").load("./current/p3/info.php");
-					}
-					if(this.id == "ci4"){
-						$("#current-content").empty();
-						$("#current-content").show();
-						$("#current-content").load("./current/p4/info.php");
-					}
-				});
-				$( ".current-item").fadeTo("slow", 0.5);
-				$( ".current-item").hover(
-					function(){
-						$(this).fadeTo( "fast", 1 );
-					}, function(){
-						$(this).fadeTo( "fast", 0.5);
-					}
-				);
-		});
-		</script>
-		
-</head>
+			<!-- Header -->
+				<header id="header">
+					<img id="logo" src="images/logo_cr_t2.png">
+					<nav id="nav">
 
-<body class="formwork">
+						<ul>
+							<li><a href="index.php#banner"     class="scrolly"> Home</a></li>
+							<li><a href="index.php#one"        class="scrolly" > Formwork</a></li>
+							<li><a href="index.php#three"      class="scrolly" > Flatwork</a></li>
+							<!-- <li><a href="index.php#two"        class="scrolly" > Paving</a></li> -->
+							<li><a href="safety.html"          class="scrolly"   > Safety</a></li>
+							<li><a href="careers.html"         class="scrolly"  > Careers</a></li>
+							<li><a href="contact.html"         class="scrolly"  > Contact</a></li>
+						</ul>
 
-	<div id="container">
-		<div id="header">
-			<?php include 'includes/header.inc' ?>
+						<ul>
+				      <li><a href="#residential"   class="scrolly"> Residential</a></li>
+				      <li><a href="#commercial"    class="scrolly"> Commercial</a></li>
+				      <li><a href="#institutional" class="scrolly"> Institutional</a></li>
+				      <li><a href="#cores"         class="scrolly"> Cores</a></li>
+				      <li><a href="#architectural" class="scrolly"> Architectural</a></li>
+    				</ul>
+
+					</nav>
+				</header>
+
+			<!-- Banner -->
+			<!--	<section id="banner">
+					<div class="content">
+
+					</div>
+					<a href="#one" class="goto-next scrolly">Next</a>
+				</section>
+			-->
+
+
+			<!-- RESIDENTIAL -->
+
+
+				<section id="residential" class="spotlight style2 right">
+					<div id="residentialpics">
+
+						<?php
+						    $dirRes  = opendir("./images/formwork/residential");
+						    $dirtxtRes = "./images/formwork/residential";
+						    while (($filenameRes = readdir($dirRes)) !==false) {
+						      $filesRes[] = $filenameRes;
+						    }
+						    $imagesRes = preg_grep ('/\.jpg$/i', $filesRes);
+						    shuffle($imagesRes);
+						    for ($i = 0; $i < count($imagesRes); $i++){
+						      echo "<div style=\"background-image: url('".$dirtxtRes."/".$imagesRes[$i]."');\"></div>";
+						    }
+								$dirRes = closedir();
+						?>
+
+						</div>
+
+
+						<div class="content">
+						<header>
+							<h2>Residential Projects</h2>
+						</header>
+						<p class="list-heading">Highrise</p>
+						<ul>
+							<li>Lamb Tower</li>
+							<li>The Hendrix</li>
+							<li>Smith</li>
+						</ul>
+						<p class="list-heading">Midrise</p>
+						<ul>
+							<li>Ezra on Riley Park</li>
+							<li>Ventus</li>
+						</ul>
+						<p class="list-heading">Multifamily (single level parkades)</p>
+						<ul>
+							<li>1741</li>
+							<li>Orchard Sky</li>
+							<li>Skymills</li>
+						</ul>
+						<ul class="actions">
+							<li><a href="#" class="button" style='display:none;'>Learn More</a></li>
+						</ul>
+					</div>
+					<a href="#commercial" class="goto-next scrolly">Next</a>
+				</section>
+
+
+			<!-- COMMERCIAL -->
+
+
+				<section id="commercial" class="spotlight style3 left">
+
+					<div id="commercialpics">
+
+						<!-- build the divs in random order -->
+						<?php
+
+								#open the working directory into $dir
+								$dirCom  = opendir("./images/formwork/commercial");
+								$dirtxtCom = "./images/formwork/commercial";
+
+								#store ALL the files in $files[]
+								while (($filenameCom = readdir($dirCom)) !==false) {
+									$filesCom[] = $filenameCom;
+								}
+								#take only .jpg into $images[]
+								$imagesCom=preg_grep ('/\.jpg$/i', $filesCom);
+								#randomize!
+								shuffle($imagesCom);
+								#spit out all the <div>s
+								for ($i = 0; $i < count($imagesCom); $i++){
+									echo "<div style=\"background-image: url('".$dirtxtCom."/".$imagesCom[$i]."');\"></div>";
+								}
+								$dirCom = closedir();
+						?>
+						</div>
+
+
+
+					<div class="content">
+						<header>
+							<h2>Commercial Projects</h2>
+						</header>
+
+						<p class="list-heading">Recent Projects</p>
+
+						<ul>
+							<li>11 Ave Place</li>
+							<li>Engineered Air Office Building</li>
+							<li>Harvest Hills Office Park</li>
+							<li>Royal Canadian Legion</li>
+							<li>Calgary Courts Center Ph2</li>
+						</ul>
+
+						<ul class="actions">
+							<li><a href="#" class="button" style='display:none;'>Learn More</a></li>
+						</ul>
+					</div>
+					<a href="#institutional" class="goto-next scrolly">Next</a>
+				</section>
+
+
+			<!-- INSTITUTIONAL -->
+
+				<section id="institutional" class="spotlight style2 right">
+
+					<div id="institutionalpics">
+
+						<!-- build the divs in random order -->
+						<?php
+
+								#open the working directory into $dir
+								$dirIns  = opendir("./images/formwork/institutional");
+								$dirtxtIns = "./images/formwork/institutional";
+
+								#store ALL the files in $files[]
+								while (($filenameIns = readdir($dirIns)) !==false) {
+									$filesIns[] = $filenameIns;
+								}
+								#take only .jpg into $images[]
+								$imagesIns=preg_grep ('/\.jpg$/i', $filesIns);
+								#randomize!
+								shuffle($imagesIns);
+								#spit out all the <div>s
+								for ($i = 0; $i < count($imagesIns); $i++){
+									echo "<div style=\"background-image: url('".$dirtxtIns."/".$imagesIns[$i]."');\"></div>";
+								}
+								$dirIns = closedir();
+						?>
+						</div>
+
+
+
+					<div class="content">
+						<header>
+							<h2>Institutional Projects</h2>
+						</header>
+
+						<p class="list-heading">Recent Projects</p>
+
+						<ul>
+							<li>University of Calgary - Student Residences</li>
+							<li>Mount Royal University - Conservatory and Bella Concert Hall</li>
+							<li>CCIT Office Expansion</li>
+							<li>Taylor Family Digital Library JV</li>
+						</ul>
+
+					</div>
+					<a href="#cores" class="goto-next scrolly">Next</a>
+				</section>
+
+
+				<!-- Cores -->
+
+					<section id="cores" class="spotlight style3 left">
+
+						<div id="corespics">
+
+							<!-- build the divs in random order -->
+							<?php
+
+									#open the working directory into $dir
+									$dirCor  = opendir("./images/formwork/cores");
+									$dirtxtCor = "./images/formwork/cores";
+
+									#store ALL the files in $files[]
+									while (($filenameCor = readdir($dirCor)) !==false) {
+										$filesCor[] = $filenameCor;
+									}
+									#take only .jpg into $images[]
+									$imagesCor=preg_grep ('/\.jpg$/i', $filesCor);
+									#randomize!
+									shuffle($imagesCor);
+									#spit out all the <div>s
+									for ($i = 0; $i < count($imagesCor); $i++){
+										echo "<div style=\"background-image: url('".$dirtxtCor."/".$imagesCor[$i]."');\"></div>";
+									}
+									$dirCor = closedir();
+							?>
+							</div>
+
+
+
+						<div class="content">
+							<header>
+								<h2>Freestanding Cores</h2>
+							</header>
+
+							<p class="list-heading">Recent Projects</p>
+
+							<ul>
+								<li>All Seniors Care - Sage Hill</li>
+								<li>All Seniors Care - Mactaggart</li>
+								<li>Westbrook Office Building</li>
+							</ul>
+
+						</div>
+						<a href="#architectural" class="goto-next scrolly">Next</a>
+					</section>
+
+
+					<!-- ARCHITECTURAL -->
+
+						<section id="architectural" class="spotlight style3 right">
+
+							<div id="architecturalpics">
+
+								<!-- build the divs in random order -->
+								<?php
+
+										#open the working directory into $dir
+										$dirArc  = opendir("./images/formwork/architectural");
+										$dirtxtArc = "./images/formwork/architectural";
+
+										#store ALL the files in $files[]
+										while (($filenameArc = readdir($dirArc)) !==false) {
+											$filesArc[] = $filenameArc;
+										}
+										#take only .jpg into $images[]
+										$imagesArc=preg_grep ('/\.jpg$/i', $filesArc);
+										#randomize!
+										shuffle($imagesArc);
+										#spit out all the <div>s
+										for ($i = 0; $i < count($imagesArc); $i++){
+											echo "<div style=\"background-image: url('".$dirtxtArc."/".$imagesArc[$i]."');\"></div>";
+										}
+										$dirArc = closedir();
+								?>
+								</div>
+
+
+
+							<div class="content">
+								<header>
+									<h2>Architectural Exposed Concrete</h2>
+								</header>
+<!--
+								<p>Recent Projects:</p>
+
+								<ul>
+									<li>All Seniors Care - Sage Hill</li>
+									<li>All Seniors Care - Mactaggart</li>
+									<li>Westbrook</li>
+								</ul>
+-->
+							</div>
+							<!-- <a href="#four" class="goto-next scrolly">Next</a>-->
+						</section>
+
+			<!-- Five -->
+			<!--
+				<section id="five" class="wrapper style2 special fade">
+					<div class="container">
+						<header>
+							<h2>About Us</h2>
+							<p>We look forward to working with you.</p>
+						</header>
+
+						<p><strong>Cemrock Concrete &amp; Construction Ltd.</strong> has been
+							involved in the cast-in-place concrete construction business since our
+							inception in 2003.</p>
+						<p>During this time we have had the pleasure of developing successful
+							relationships with Alberta's construction leaders. We conduct our
+							business in a professional manner and have developed a reputation for
+							providing exceptional concrete work.</p>
+						<p>We continue to build on our reputation through our:<br>
+						<ul>
+							<li style="list-style:none;">COR certified safety program (Alberta Construction Safety Association)</li>
+							<li style="list-style:none;">Experienced and Knowledgeable Field Personnel</li>
+							<li style="list-style:none;">Quick Response Estimating (Budgets, Bidding, Revisions)</li>
+							<li style="list-style:none;">Thorough Planning/Organization (Schedules, Submissions)</li>
+						</ul>
+						<br>
+						<br>
+
+
+					</div>
+				</section>
+-->
+			<!-- Footer -->
+				<footer id="footer">
+					<!--
+					<ul class="icons">
+						<li><a href="https://twitter.com/cemrockcc" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/Cemrock-Concrete-Construction-244353132603513/" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.linkedin.com/company/cemrock-concrete-&-construction/" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+					</ul>
+				-->
+					<ul class="copyright">
+						<li>&copy; Cemrock. All rights reserved.</li>
+					</ul>
+				</footer>
+
 		</div>
 
-	<div id="content">
-		<!--############################# -->
-		<!--#########  CONTENT ########## -->
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
 
 
-		<h1>Current Formwork Projects</h1>
 
-		<!--<a id="slink" class="icon-share" href="current.php">Current Projects</a>-->
-		<div id="current-content">
-			<!-- leprechaun's droppings here  -->
-		</div>
-		<br><br>
-		<div id="current-container">
-			<div class="current-item" id="ci1"> <img src="./current/p1/thumb.jpg"/>11 Av.<br><br></div>
-			<div class="current-item" id="ci2"> <img src="./current/p2/thumb.jpg"/>UofC/7<br><br></div>
-			<div class="current-item" id="ci3"> <img src="./current/p3/thumb.jpg"/>UofC/8<br><br></div>
-			<div class="current-item" id="ci4"> <img src="./current/p4/thumb.jpg"/>Mahogany<br><br></div>
-		</div>
-		<br><br>
-		<p><strong>Cemrock Formwork Division</strong> has successfully completed a wide array of concrete projects including:</p>
+			<script>
+			window.onscroll = function() {myFunction()};
 
-		<p><u>Commercial Projects (including LEED projects)</u></p>
-		<ul>
-			<li>Multilevel office developments</li>
-			<li>Parkades</li>
-			<li>Warehouse / Retail Buildings</li>
-			<li>Foundations</li>
-			<li>Retaining Walls</li>
-		</ul>
-		<p><u>Institutional Projects</u></p>
-		<ul>
-			<li>Municipal Facilities</li>
-			<li>College / University Facilities or Buildings</li>
-			<li>Schools (K - 12)</li>
-		</ul>
-		<p><u>Multidwelling Residential Projects</u></p>
-		<ul>
-			<li> Multilevel condominium development</li>
-		</ul>
-		<p><u>Industrial Projects</u></p>
-		<ul>
-			<li>Calgary West LRT</li>
-			<li>Water Treatment Plants</li>
-			<li>City of Airdrie Northeast Reservoir & Pump Station</li>
-		</ul>
+			var header = document.getElementById("header");
+			var sticky = header.offsetTop;
+			//console.log(header.offsetTop);
 
-	<!--#########  /CONTENT ######### -->
-	<!--############################# -->
-	</div>
+			function myFunction() {
 
-	<div id="footer">
-		<?php include 'includes/footer.inc' ?>
-	</div>
+			    header.classList.add("sticky");
 
-</div>
+			}
+			</script>
 
-</body>
+			<script>
+			  function slideSwitchRes() {
+			    var $active = $('#residentialpics div.main');
+			    if ( $active.length == 0 ) $active = $('#residentialpics div:last');
+			    var $next =  $active.next().length ? $active.next()
+			      : $('#residentialpics div:first');
+			    $active.addClass('last-main');
+			    $next.css({opacity: 0.0})
+			      .addClass('main')
+			      .animate({opacity: 1.0}, 750, function() {
+			        $active.removeClass('main last-main');
+			      });
+			  }
+
+				function slideSwitchCom() {
+			    var $active = $('#commercialpics div.main');
+			    if ( $active.length == 0 ) $active = $('#commercialpics div:last');
+			    var $next =  $active.next().length ? $active.next()
+			      : $('#commercialpics div:first');
+			    $active.addClass('last-main');
+			    $next.css({opacity: 0.0})
+			      .addClass('main')
+			      .animate({opacity: 1.0}, 750, function() {
+			        $active.removeClass('main last-main');
+			      });
+			  }
+
+				function slideSwitchIns() {
+			    var $active = $('#institutionalpics div.main');
+			    if ( $active.length == 0 ) $active = $('#institutionalpics div:last');
+			    var $next =  $active.next().length ? $active.next()
+			      : $('#institutionalpics div:first');
+			    $active.addClass('last-main');
+			    $next.css({opacity: 0.0})
+			      .addClass('main')
+			      .animate({opacity: 1.0}, 750, function() {
+			        $active.removeClass('main last-main');
+			      });
+			  }
+
+				function slideSwitchCor() {
+			    var $active = $('#corespics div.main');
+			    if ( $active.length == 0 ) $active = $('#corespics div:last');
+			    var $next =  $active.next().length ? $active.next()
+			      : $('#corespics div:first');
+			    $active.addClass('last-main');
+			    $next.css({opacity: 0.0})
+			      .addClass('main')
+			      .animate({opacity: 1.0}, 750, function() {
+			        $active.removeClass('main last-main');
+			      });
+			  }
+
+				function slideSwitchArc() {
+			    var $active = $('#architecturalpics div.main');
+			    if ( $active.length == 0 ) $active = $('#architecturalpics div:last');
+			    var $next =  $active.next().length ? $active.next()
+			      : $('#architecturalpics div:first');
+			    $active.addClass('last-main');
+			    $next.css({opacity: 0.0})
+			      .addClass('main')
+			      .animate({opacity: 1.0}, 750, function() {
+			        $active.removeClass('main last-main');
+			      });
+			  }
+
+			  $(function() {
+			    setInterval( "slideSwitchRes()", 4100 );
+					setInterval( "slideSwitchCom()", 3800 );
+					setInterval( "slideSwitchIns()", 3950 );
+					setInterval( "slideSwitchCor()", 4000 );
+					setInterval( "slideSwitchArc()", 3850 );
+			  });
+			</script>
+
+	</body>
 </html>
